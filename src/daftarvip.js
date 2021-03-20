cf3bb6c882801079955f365e4537e4f96c7840d6
diego325 <80598034+diego325@users.noreply.github.com>
2021-03-20 02:55:04 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,8 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+Da uma mamada pra ter premiun ksskksks  `
+}
+exports.daftarvip = daftarvip
---
+++ src/help1.js
@@ -0,0 +1,9 @@
+const help1 = (prefix) => {
+	return `
+┃MENU EM ATUALIZAÇÃO ⚠️
+┣QUALQUER COISA
+┣CHAME O DONO
+┣wa.me/+5511998346168
+}
+exports.help1 = help1
+
