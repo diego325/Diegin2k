2fb6f9a859cc4ee227c1045ffed3888a0eb5a782
diego325 <80598034+diego325@users.noreply.github.com>
2021-03-22 05:32:34 -0300

    Add files via upload

---
+++ dark/help.js
@@ -0,0 +1,369 @@
+const help = (prefix) => { 
+	return `
+⚜〘 INFO 〙⚜
+
+OLÁ base feita por dark
+
+➥๛𝙳𝙸𝙴𝙶𝙸𝙽𝟸𝙺𖨐  𝘽𝙊𝙏
+➥ 3.2
+➥ 𝐃𝐎𝐍𝐎:  ⃬⃗๛𝙳𝙸𝙴𝙶𝙸𝙽𝟸𝙺𖨐 𒋨
+➥ wa.me/+5511998346168 𒋨
+➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+⚜〘 NOVIDADES 〙⚜
+
+ 𒋨${prefix}msc𒋨
+ 𒋨${prefix}Comandos de voz𒋨
+ 𒋨${prefix}meme [corrigido]𒋨
+ 𒋨${prefix}comandos sem prefix𒋨
+ 𒋨${prefix}gbin [premium]𒋨
+ 𒋨${prefix}pack [premium]𒋨
+ 𒋨${prefix}destrava [premium]𒋨
+ 𒋨${prefix}gpessoa [premium]𒋨
+ 𒋨${prefix}wame𒋨
+ 𒋨${prefix}spamcall𒋨
+ 𒋨${prefix}play (nome da msc)𒋨
+
+⚜〘 MENU 〙⚜
+
+ 𒋨${prefix}figu𒋨
+ 𒋨${prefix}toimg𒋨
+ 𒋨${prefix}darkjokes (memes aleatórios)𒋨
+ 𒋨${prefix}memeindo𒋨
+ 𒋨${prefix}tts𒋨
+ 𒋨${prefix}lolih [on]𒋨
+ 𒋨${prefix}nsfwloli [off]𒋨
+ 𒋨${prefix}url2img𒋨
+ 𒋨${prefix}leens [na legenda]𒋨
+ 𒋨${prefix}wait [na legenda]𒋨
+ 𒋨${prefix}setprefix𒋨
+
+⚜〘 OUTROS 〙⚜
+
+ 𒋨${prefix}linkgp𒋨
+ 𒋨${prefix}simih [1/0]𒋨
+ 𒋨${prefix}marcar𒋨
+ 𒋨${prefix}add [@]𒋨
+ 𒋨${prefix}banir [@]𒋨
+ 𒋨${prefix}promover [@]𒋨
+ 𒋨${prefix}rebaixar𒋨
+ 𒋨${prefix}admins𒋨
+ 𒋨${prefix}marcar2𒋨
+ 𒋨${prefix}bc [texto]𒋨 (ele faz uma ™)
+ 𒋨${prefix}marcar3𒋨
+ 𒋨${prefix}bloqueados𒋨
+ 𒋨${prefix}bloquear [@]𒋨
+ 𒋨${prefix}desbloquear [@]𒋨
+ 𒋨${prefix}limpar𒋨
+ 𒋨${prefix}bc [ 𒋨texto𒋨 ]𒋨
+ 𒋨${prefix}bemvindo [1/0]𒋨
+ 𒋨${prefix}clonar [@]𒋨
+ 𒋨${prefix}help1𒋨
+ 𒋨${prefix}dono𒋨
+ 𒋨${prefix}owner𒋨
+ 𒋨${prefix}tts [texto]𒋨
+ 𒋨${prefix}setnome𒋨
+ 𒋨${prefix}termux𒋨
+ 𒋨${prefix}setfoto𒋨
+ 𒋨${prefix}grupoinfo𒋨
+ 𒋨${prefix}ytmp4𒋨
+ 𒋨${prefix}bomdia𒋨
+ 𒋨${prefix}boanoite𒋨
+ 𒋨${prefix}marcar𒋨
+ 𒋨${prefix}marcar2𒋨
+ 𒋨${prefix}marcar3𒋨
+
+⚜〘 IMAGENS 〙⚜
+
+ 𒋨${prefix}loli𒋨 [off]
+ 𒋨${prefix}loli1𒋨
+ 𒋨${prefix}hentai𒋨
+ 𒋨${prefix}dono𒋨
+ 𒋨${prefix}porno𒋨
+ 𒋨${prefix}boanoite𒋨
+ 𒋨${prefix}bomdia𒋨
+ 𒋨${prefix}boatarde𒋨
+ 𒋨${prefix}mia [aleatórias]𒋨
+ 𒋨${prefix}rize [aleatórias]𒋨
+ 𒋨${prefix}minato [aleatórias]𒋨
+ 𒋨${prefix}boruto [aleatórias]𒋨
+ 𒋨${prefix}hinata [aleatórias]𒋨
+ 𒋨${prefix}sasuke [aleatórias]𒋨
+ 𒋨${prefix}sakura [aleatórias]𒋨
+ 𒋨${prefix}naruto [aleatórias]𒋨
+ 𒋨${prefix}meme𒋨   
+ 𒋨${prefix}lofi𒋨
+ 𒋨${prefix}malkova𒋨
+ 𒋨${prefix}canal𒋨
+ 𒋨${prefix}nsfwloli1𒋨
+ 𒋨${prefix}reislin𒋨
+
+⚜〘 INTELIGÊNCIA IA 〙⚜
+
+ 𒋨${prefix}simih 1 (para ativar)𒋨
+ 𒋨${prefix}simih 0 (para desativar)𒋨
+ 𒋨${prefix}simi (sua mensagem)𒋨
+
+⚜〘 EM TESTE 〙⚜
+
+ 𒋨${prefix}𒋨
+ 𒋨${prefix}𒋨
+ 𒋨${prefix}𒋨
+
+⚜〘 PREMIUM 〙⚜
+
+ 𒋨${prefix}dado𒋨
+ 𒋨${prefix}cekvip𒋨
+ 𒋨${prefix}premiumlist𒋨
+ 𒋨${prefix}delete𒋨
+ 𒋨${prefix}modapk𒋨
+ 𒋨${prefix}indo10𒋨
+ 𒋨${prefix}daftarvip [para virar Premium]𒋨
+ 𒋨${prefix}qrcode𒋨
+ 𒋨${prefix}chentai𒋨
+ 𒋨${prefix}gcpf𒋨
+ 𒋨${prefix}gbin𒋨
+ 𒋨${prefix}pack𒋨
+ 𒋨${prefix}destrava𒋨
+ 𒋨${prefix}gpessoa𒋨
+
+⚜〘 GRUPO 〙⚜
+
+ 𒋨${prefix}banir𒋨
+ 𒋨${prefix}leveling [on/off]𒋨
+ 𒋨${prefix}level𒋨
+ 𒋨${prefix}add𒋨
+ 𒋨${prefix}promover𒋨
+ 𒋨${prefix}setfoto [na legenda]𒋨
+ 𒋨${prefix}setname [texto]𒋨
+ 𒋨${prefix}rebaixar𒋨
+ 𒋨${prefix}admins𒋨
+ 𒋨${prefix}marcar𒋨
+ 𒋨${prefix}marcar2𒋨
+ 𒋨${prefix}marcar3𒋨
+ 𒋨${prefix}bemvindo [1/0]𒋨
+ 𒋨${prefix}grupoinfo𒋨
+ 𒋨${prefix}bomdia𒋨
+ 𒋨${prefix}boatarde𒋨
+ 𒋨${prefix}boanoite𒋨
+ 𒋨${prefix}setdesc𒋨
+ 𒋨${prefix}bug [sua mensagem]𒋨
+   𒋨${prefix}tag 𒋨
+
+⚜〘 ESPECIFICO DO BOT 〙⚜
+
+ 𒋨${prefix}bug [sua mensagem]𒋨
+ 𒋨${prefix}clonar [@]𒋨
+ 𒋨${prefix}dono𒋨
+ 𒋨${prefix}ping [ver velocidade do bot]𒋨
+ 𒋨${prefix}termux𒋨
+ 𒋨${prefix}gay [@]𒋨
+ 𒋨${prefix}wame𒋨
+ 𒋨${prefix}map (nome)𒋨
+ 𒋨${prefix}setppbot (marque uma img)𒋨
+ 𒋨${prefix}pinterest (nome)𒋨
+ 𒋨${prefix}desligar (so para o dono)𒋨
+ 𒋨${prefix}timer𒋨
+
+⚜〘 MAIS ALGUNS 〙⚜
+
+ 𒋨${prefix}neko𒋨
+ 𒋨${prefix}ttp [texto]𒋨
+ 𒋨${prefix}testime𒋨
+ 𒋨${prefix}tomp3𒋨
+ 𒋨${prefix}modoanime [on/off]𒋨
+ 𒋨${prefix}modonsfw [on/off]𒋨
+ 𒋨${prefix}happymod [jogo/app]𒋨
+ 𒋨${prefix}rize𒋨
+ 𒋨${prefix}ytsearch𒋨
+ 𒋨${prefix}moddroid [jogo/app]𒋨
+ 𒋨${prefix}xvideos [titulo]𒋨𒋨
+ 𒋨${prefix}nomegp𒋨
+ 𒋨${prefix}darkjokes (memes aleatórios)𒋨
+ 𒋨${prefix}animecry𒋨
+ 𒋨${prefix}gay1𒋨
+ 𒋨${prefix}next𒋨
+ 𒋨${prefix}alerta𒋨
+ 𒋨${prefix}belle [img aleatórias]𒋨
+ 𒋨${prefix}pronomeneu [texto]𒋨
+ 𒋨${prefix}hobby𒋨
+
+⚜〘 COMANDOS DE VOZ 〙⚜
+
+ 𒋨${prefix}ola𒋨
+ 𒋨${prefix}bv𒋨
+ 𒋨${prefix}tchau𒋨
+ 𒋨${prefix}bem𒋨
+ 𒋨${prefix}a𒋨
+ 𒋨${prefix}fdp𒋨
+ 𒋨${prefix}onich𒋨
+ 𒋨${prefix}beat1𒋨
+ 𒋨${prefix}glub𒋨
+
+⚜〘 OUTROS /2 〙⚜
+
+ 𒋨${prefix}antilink [1/0]𒋨
+ 𒋨${prefix}brainly [pergunta]𒋨
+ 𒋨${prefix}antiracismo [on/off]𒋨
+ 𒋨${prefix}setnomebot𒋨
+ 𒋨${prefix}meme𒋨
+ 𒋨${prefix}musicas𒋨
+
+⚜〘 YT MUSIC 〙⚜
+
+══NOTA »
+menu completo em .msc
+══════════════════
+
+ 𒋨${prefix}hylander𒋨
+ 𒋨${prefix}nikeb𒋨
+ 𒋨${prefix}BanhoDeLeite𒋨
+ 𒋨${prefix}AkDoFlamengo𒋨
+ 𒋨${prefix}Nemo𒋨
+ 𒋨${prefix}kalidade𒋨
+ 𒋨${prefix}macaverde𒋨
+ 𒋨${prefix}m4𒋨
+ 𒋨${prefix}cmgremio𒋨
+ 𒋨${prefix}cmflamengo𒋨
+ 𒋨${prefix}mercury𒋨
+ 𒋨${prefix}ponta𒋨
+ 𒋨${prefix}moça𒋨
+ 𒋨${prefix}nav𒋨
+ 𒋨${prefix}dama𒋨
+ 𒋨${prefix}remedy𒋨
+ 𒋨${prefix}cavala𒋨
+ 𒋨${prefix}facetime𒋨
+ 𒋨${prefix}pe𒋨
+ 𒋨${prefix}nike12𒋨
+
+
+⚜〘 HACKER MENU 〙⚜
+
+ 𒋨${prefix}fbcheker𒋨
+ 𒋨${prefix}hackfb𒋨
+ 𒋨${prefix}bruteforcefb𒋨
+ 𒋨${prefix}toolsphishing𒋨
+ 𒋨${prefix}terkeytermux𒋨
+ 𒋨${prefix}socialfishv2𒋨
+ 𒋨${prefix}tool𒋨
+
+⚜〘 STICKER MENU 〙⚜
+
+ 𒋨${prefix}animecry𒋨 
+ 𒋨${prefix}hubgif𒋨
+ 𒋨${prefix}blowgif𒋨 🔞
+ 𒋨${prefix}shota𒋨
+ 𒋨${prefix}hentaifig𒋨 🔞
+ 𒋨${prefix}nekofig𒋨 🔞
+ 𒋨${prefix}lolifig𒋨
+
+⚜〘 INTERATIVOS 〙⚜
+
+══NOTA »
+Mandar a msg sem o prefixo
+════════════════════
+
+ 𒋨bah𒋨
+ 𒋨oii𒋨
+ 𒋨bv𒋨
+ 𒋨canta ai bot𒋨
+ 𒋨grita𒋨
+ 𒋨digita𒋨
+ 𒋨machista𒋨
+
+⚜〘 OUTROS /3 〙⚜
+
+ 𒋨${prefix}atris𒋨 
+ 𒋨${prefix}animemenu𒋨
+
+⚜〘 CMDS DE VÍDEO 〙⚜
+
+══EDITS »
+_𒋨Say Cat𒋨_
+════════
+
+ 𒋨${prefix}say1𒋨
+ 𒋨${prefix}say2𒋨
+ 𒋨${prefix}say3𒋨
+ 𒋨${prefix}say4𒋨
+
+══EDITS »
+_𒋨XXXTENTACIONS𒋨_
+═══════════════
+
+ 𒋨${prefix}xxx1𒋨
+ 𒋨${prefix}xxx2𒋨
+
+══EDITS »
+_𒋨VARIADAS𒋨_
+═══════════════
+
+ 𒋨${prefix}teto1𒋨
+ 𒋨${prefix}edit1𒋨
+ 𒋨${prefix}edit2𒋨
+ 𒋨${prefix}edit3𒋨
+ 𒋨${prefix}edit4𒋨
+═══════════════
+
+⚜〘 CONSULTAS E OUTROS 〙⚜
+
+
+ 𒋨${prefix}iplog (ip)𒋨 [consulta ip]
+ 𒋨${prefix}cpfg𒋨 [bot gera cpf]
+ 𒋨${prefix}bcep (cep)𒋨 [consulta cep]
+
+⚜〘 LOGOS 〙⚜
+
+ 𒋨${prefix}pslogo (seu texto)𒋨
+ 𒋨${prefix}milogo (seu texto)𒋨
+ 𒋨${prefix}aguia (seu texto)𒋨
+ 𒋨${prefix}monkey (seu texto)𒋨
+ 𒋨${prefix}dnulis (seu texto)𒋨
+ 𒋨${prefix}dmeme (seu texto)𒋨
+ 𒋨${prefix}dnobg (seu texto)𒋨
+ 𒋨${prefix}dnobg2 (seu texto/seu texto)𒋨
+ 𒋨${prefix}tlogo (seu texto)𒋨
+ 𒋨${prefix}nlogo (seu texto)𒋨
+ 𒋨${prefix}black (seu texto)𒋨
+ 𒋨${prefix}clogo (seu texto)𒋨
+ 𒋨${prefix}glogo (seu texto/seu texto)𒋨
+ 𒋨${prefix}aguia2 (seu texto/seu texto)𒋨
+ 𒋨${prefix}wlogo (seu texto)𒋨
+ 𒋨${prefix}letxt (seu texto)𒋨
+ 𒋨${prefix}text3d (seu texto)𒋨
+ 𒋨${prefix}amor (tamanho/blush)𒋨 [numeros]
+ 𒋨${prefix}frase (seu texto/seu texto)𒋨
+ 𒋨${prefix}canal (seu texto)𒋨
+ 𒋨${prefix}plogo (seu texto)𒋨 [plaquinha]
+
+⚜〘 MUSIC GEEK 〙⚜
+
+ 𒋨${prefix}minato1
+ 𒋨${prefix}minato2
+ 𒋨${prefix}deidara
+ 𒋨${prefix}hashirama
+ 𒋨${prefix}pain
+ 𒋨${prefix}vegeto
+ 𒋨${prefix}gogeta
+ 𒋨${prefix}eren
+ 𒋨${prefix}renegado
+ 𒋨${prefix}tobi
+ 𒋨${prefix}vibea
+ 𒋨${prefix}tobirama
+
+⚜〘 𝗗𝗢𝗡𝗢 〙⚜
+
+ 𒋨NOME: DIEGO O BRABO  𒋨
+ 𒋨INSTA: zonoxxdesaigner_
+ 𒋨WPP: wa.me/+5511998346168 𒋨
+ 𒋨YOUTUBE: ZonoxX𒋨
+
+ 𒋨"kibadores, vão se fuder"𒋨
+  
+ 𒋨${prefix}dono𒋨 para mais info
+
+〘  ๛𝙳𝙸𝙴𝙶𝙸𝙽𝟸𝙺𖨐 𝘽𝙊𝙏 〙
+`
+}
+
+exports.help = help
+
