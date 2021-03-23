8ee87889662fc74458f65ee3c31b0bba8355a560
diego325 <80598034+diego325@users.noreply.github.com>
2021-03-22 21:48:48 -0300

    Add files via upload

---
+++ src/pack.js
@@ -0,0 +1,22 @@
+const pack = (prefix) => {
+    return `*PACKS:*
+    
+*DARK DOMIMA 🐊🚩*
+
+Belle delphine : https://photos.app.goo.gl/Jr4Qk1dFSJepPdRc7 
+
+Pack1: https://photos.app.goo.gl/phyRpNFLcAtsknfJ7 
+
+Kitty kum: https://photos.app.goo.gl/gNxLbJHGVzeWY9iS9 
+
+Misaki Your Waifu: https://photos.app.goo.gl/M9nyHdonAJu5GRjZ9 
+
+pack: https://photos.app.goo.gl/SrogbCbnanL2PJQn7 
+
+Love Lilah : https://photos.app.goo.gl/TBbkjGGdAVHjPFt5A 
+
+https://photos.app.goo.gl/85qYeWHLiM6dBp6x7
+
+`
+}
+exports.pack = pack
